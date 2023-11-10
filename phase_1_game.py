7bdc3c16bea50a093034bbbc0c7d17aac9e1523a
ketsia <ketsia.donfack@gmail.com>
2023-11-10 15:04:34 -0500

    Completed code for Phase 1: Our Dinosaur Game with Volcano

--- phase_1_game.py
+++ phase_1_game.py
@@ -13,16 +13,16 @@ class World:
     trex_speed: int
     is_jumping: bool
     jump_height: int
+    platforms: list[DesignerObject]
 
 
 def create_world() -> World:
-    return World(create_volcano(), create_trex(), TREX_SPEED, False, 0)
+    return World(create_volcano(), create_trex(), TREX_SPEED, False, 0, create_platforms())
 
 
 def create_volcano() -> DesignerObject:
     volcano = emoji("🌋")
     volcano.y = get_height() * (.59)
-    volcano.flip_x = True
     volcano.scale_x = 25
     volcano.scale_y = 20
     return volcano
@@ -37,9 +37,36 @@ def create_trex() -> DesignerObject:
     return trex
 
 
+def create_platforms() -> list[DesignerObject]:
+    platforms = []
+    count = 10
+    while count > 0:
+        platform = rectangle("purple", 100, 20)
+        platform.x = get_width() * (randint(1, 10) * .1)
+        platform.y = get_height() * (randint(1, 10) * .1)
+        platforms.append(platform)
+        count -= 1
+
+    return platforms
+
+
+def check_platform_collision(world: World):
+    for platform in world.platforms:
+        if (
+                world.trex.x < platform.x + platform.width
+                and world.trex.x + world.trex.width > platform.x
+                and world.trex.y < platform.y + platform.height
+                and world.trex.y + world.trex.height > platform.y
+        ):
+            if not world.is_jumping:
+                world.trex.y = platform.y - world.trex.height * 0.7
+            return True
+    return False
+
+
 def jump_trex(world: World, key: str):
     """Make the trex jump"""
-    if key == "space":
+    if key == "space" and not world.is_jumping:
         world.is_jumping = True
         world.jump_height = JUMP_HEIGHT
 
@@ -63,6 +90,7 @@ def move_trex(world: World):
             world.is_jumping = False
     elif world.trex.y < get_height() * (.93):
         world.trex.y += 10
+    check_platform_collision(world)
 
 
 def flip_trex(world: World, key: str):
@@ -77,6 +105,9 @@ def wall_pow(world: World):
         head_left(world)
     elif world.trex.x < 0:
         head_right(world)
+    if world.trex.y < 0:
+        world.trex.y = 0
+        world.is_jumping = False
 
 
 when('starting', create_world)
@@ -86,6 +117,3 @@ when("typing", jump_trex)
 when("updating", wall_pow)
 
 start()
-
-
-
